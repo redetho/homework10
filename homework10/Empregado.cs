9625b76295fa2452641943f0faf492d4aff8ff6b
Thomas Redekop <redetho@gmail.com>
2021-10-27 16:26:59 -0300

    Adicionar arquivos de projeto.

---
+++ homework10/Empregado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework10
+{
+    class Empregado
+    {
+        public string Nome { get; set; }
+
+        public int Id { get; set; }
+
+        public double Salario { get; set; }
+
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = Salario * porcentagem;
+        }
+    }
+}
---
+++ homework10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero:");
+            int numero = int.Parse(Console.ReadLine());
+
+            List<Empregado> todosfunc = new List<Empregado>();
+
+            for (int i = 0; i < numero; i++)
+            { 
+                Console.WriteLine("Digite seu nome:");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite seu id:");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite seu salário");
+                double salario = double.Parse(Console.ReadLine());
+
+                todosfunc.Add(new Empregado { Id = id, Nome = nome, Salario = salario });
+            }
+
+            Console.WriteLine("Digite o id de um funcionário:");
+            int idFuncionario = int.Parse(Console.ReadLine());
+
+            var funcionario = todosfunc.Find(num => num.Id == idFuncionario);
+
+            if (funcionario != null)
+            {
+                Console.WriteLine("Digite a porcentagem de aumento do salário:");
+                double porcentagem = double.Parse(Console.ReadLine());
+                funcionario.AumentarSalario(porcentagem);
+            }
+            else if (funcionario == null)
+            {
+                Console.WriteLine("Esse ID nao existe.");
+            }
+
+            Console.WriteLine("Salarios e funcionarios:");
+            foreach (var bagulho in todosfunc)
+            {
+                Console.WriteLine(bagulho.Nome);
+                Console.WriteLine(bagulho.Id);
+                Console.WriteLine(bagulho.Salario);
+                Console.WriteLine();
+            }
+        }
+    }
+}
