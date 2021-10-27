632e0398fc4c40794871fc0c7bd6dfb2cd161642
Thomas Redekop <redetho@gmail.com>
2021-10-27 16:33:13 -0300

    comentários

--- homework10/Empregado.cs
+++ homework10/Empregado.cs
@@ -6,13 +6,14 @@ namespace homework10
 {
     class Empregado
     {
+        //get e set
         public string Nome { get; set; }
 
         public int Id { get; set; }
 
         public double Salario { get; set; }
 
-
+        //função aumentar salário, aumenta de acordo com a porcentagem citada no programa.
         public void AumentarSalario(double porcentagem)
         {
             Salario = Salario * porcentagem;
--- homework10/Program.cs
+++ homework10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//usar lists
 using System.Collections.Generic;
 
 namespace homework10
@@ -7,11 +8,12 @@ namespace homework10
     {
         static void Main(string[] args)
         {
+            //recebe o numero de empregados/funcionarios
             Console.WriteLine("Digite um numero:");
             int numero = int.Parse(Console.ReadLine());
-
+            
             List<Empregado> todosfunc = new List<Empregado>();
-
+            //cria um ''empregado'' para cada numero entregue, recebe nome, id e salário
             for (int i = 0; i < numero; i++)
             { 
                 Console.WriteLine("Digite seu nome:");
@@ -22,26 +24,27 @@ namespace homework10
 
                 Console.WriteLine("Digite seu salário");
                 double salario = double.Parse(Console.ReadLine());
-
+                
                 todosfunc.Add(new Empregado { Id = id, Nome = nome, Salario = salario });
             }
-
+            //recebe o id do funcionário
             Console.WriteLine("Digite o id de um funcionário:");
             int idFuncionario = int.Parse(Console.ReadLine());
-
+            //usa "Find" para encontrar o id correspondente de um funcionario/empregado
             var funcionario = todosfunc.Find(num => num.Id == idFuncionario);
-
+            //se não for nulo, apresenta a opção de aumento de salário
             if (funcionario != null)
             {
                 Console.WriteLine("Digite a porcentagem de aumento do salário:");
                 double porcentagem = double.Parse(Console.ReadLine());
                 funcionario.AumentarSalario(porcentagem);
             }
+            //se for nulo, avisa que o ID nao existe e apresenta os empregados
             else if (funcionario == null)
             {
                 Console.WriteLine("Esse ID nao existe.");
             }
-
+            //apresenta, nome, id e salário, pula uma linha para separar os funcionários e auxiliar na leitura.
             Console.WriteLine("Salarios e funcionarios:");
             foreach (var bagulho in todosfunc)
             {
